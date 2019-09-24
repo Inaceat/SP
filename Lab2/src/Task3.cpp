c75437e5756df2d9cc8102ddd508cf92476e122f
Inaceat <inaceat@gmail.com>
2019-09-24 08:43:13 +0300

    Task3 added.

--- Lab2/src/Task3.cpp
+++ Lab2/src/Task3.cpp
@@ -4,5 +4,10 @@
 
 void Task3::Do()
 {
+	std::cout << "Открывать Проводник сюда:" << std::endl;
 
+	std::string path;
+	std::cin >> path;
+
+	ShellExecuteA(NULL, "explore", ("\"" + path + "\"").c_str(), NULL, NULL, SW_SHOWNORMAL);
 }
