8fc8099198be2659b789bb6b9eb14ff420aaab93
Inaceat <inaceat@gmail.com>
2019-09-24 08:29:07 +0300

    Task1 completed.

--- Lab2/src/Task1.cpp
+++ Lab2/src/Task1.cpp
@@ -1,8 +1,18 @@
 #include "stdafx.h"
 
+#include <string>
+
 #include "Tasks.hpp"
 
 void Task1::Do()
 {
-	
+	std::cout << "Открывать это в блокноте:" << std::endl;
+
+	std::string fileName;
+	std::cin >> fileName;
+
+
+	const std::string notepadExe = "C:\\Windows\\notepad.exe";
+
+	WinExec((notepadExe + " \"" + fileName + "\"").c_str(), SW_SHOWNORMAL);
 }
