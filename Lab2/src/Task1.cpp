f3a6ac2d6715ed212e0f32be312fa908311c871c
Inaceat <inaceat@gmail.com>
2019-09-24 15:20:28 +0300

    Moving

--- Lab2/src/Task1.cpp
+++ Lab2/src/Task1.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 
 #include "Tasks.hpp"
+#include <minwinbase.h>
+#include <minwinbase.h>
 
 void Task1::Do()
 {
@@ -9,14 +11,21 @@ void Task1::Do()
 	std::string fileName;
 	std::cin >> fileName;
 
-	std::string notepadExe = "C:\\Windows\\notepad.exe ";
-	notepadExe += " \"" + fileName + "\"";
+	//std::string notepadExe = "C:\\Windows\\notepad.exe ";
+	//notepadExe += " \"" + fileName + "\"";
+
+	LPCSTR appName = "C:\\Windows\\notepad.exe";
+	LPSTR cLine = const_cast<char*>("C:\\Windows\\notepad.exe E:\\Documents\\Coding\\Projects\\SP\\Lab2\\src\\Task1.cpp");
+
 
 	STARTUPINFOA si;
+	ZeroMemory(&si, sizeof(si));
 	si.cb = sizeof(si);
 
 	PROCESS_INFORMATION pi;
-	CreateProcessA(notepadExe.c_str(), NULL, NULL, NULL, FALSE, DETACHED_PROCESS, NULL, NULL, &si, &pi);
+	ZeroMemory(&pi, sizeof(pi));
+
+	CreateProcessA(NULL, cLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
 
 	//WinExec((notepadExe + " \"" + fileName + "\"").c_str(), SW_SHOWNORMAL);
 }
