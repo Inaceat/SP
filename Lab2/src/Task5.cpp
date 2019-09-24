95e05bd2af21bd6912c06db6f9dd8346778a3294
Inaceat <inaceat@gmail.com>
2019-09-24 09:04:42 +0300

    Task5 added.

--- Lab2/src/Task5.cpp
+++ Lab2/src/Task5.cpp
@@ -4,5 +4,39 @@
 
 void Task5::Do()
 {
-	
+	std::cout << "Изменять приоритет номера:" << std::endl;
+
+	int pID;
+	std::cin >> pID;
+
+	std::cout << "Ставить приоритет номера " << pID << " такой:" << std::endl;
+
+	int neededPriority;
+	std::cin >> neededPriority;
+
+	int priorityToSet = NORMAL_PRIORITY_CLASS;
+
+	switch (neededPriority)
+	{
+		case 0:
+			priorityToSet = NORMAL_PRIORITY_CLASS;
+			break;
+
+		case 1:
+			priorityToSet = ABOVE_NORMAL_PRIORITY_CLASS;
+			break;
+
+		case 2:
+			priorityToSet = BELOW_NORMAL_PRIORITY_CLASS;
+			break;
+		
+		default:
+			std::cout << "Unknown priority!" << std::endl;
+	}
+
+
+
+	HANDLE victim = OpenProcess(PROCESS_SET_INFORMATION, FALSE, pID);
+
+	SetPriorityClass(victim, priorityToSet);
 }
