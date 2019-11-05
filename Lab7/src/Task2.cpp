780732e3ef84aa78eb66df33e1549636847a575e
Inaceat <inaceat@gmail.com>
2019-11-05 14:25:14 +0300

    [WIP] Lab7 trying to start another instance.

--- Lab7/src/Task2.cpp
+++ Lab7/src/Task2.cpp
@@ -5,5 +5,13 @@
 
 void Task2::Do()
 {
-    
+	STARTUPINFO si;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	si.dwFlags |= CREATE_NEW_CONSOLE;
+
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&pi, sizeof(pi));
+
+	CreateProcess(L"../x64/Debug/Lab7.exe", NULL, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
 }
