c2af3af7448dcf9e765de7bcfe6ba0adb8aee6fb
Inaceat <inaceat@gmail.com>
2019-11-05 13:52:54 +0300

    Lab7 Task1 now prints files in specified directory.

--- Lab7/src/Task1.cpp
+++ Lab7/src/Task1.cpp
@@ -5,5 +5,36 @@
 
 void Task1::Do()
 {
-	
+	std::wstring directoryToList;
+
+	std::wcout << "Вводить папку для показать файлы:" << std::endl;
+	std::wcin >> directoryToList;
+
+	directoryToList.append(L"/*");
+
+
+	WIN32_FIND_DATA searchData;
+	HANDLE searchHandle = FindFirstFile(directoryToList.c_str(), &searchData);
+
+	if (INVALID_HANDLE_VALUE != searchHandle)
+	{
+		do
+		{
+			if (false == (searchData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+			{
+				std::wstring fileName(searchData.cFileName);
+
+				std::wcout << L"  " << fileName << std::endl;
+			}
+		}
+		while (FindNextFile(searchHandle, &searchData));
+
+		FindClose(searchHandle);
+	}
+	else
+	{
+		std::wcout << "Неудача... \"" << directoryToList << "\" нинайдена." << std::endl;
+	}
+
+	std::wcout << std::endl;
 }
