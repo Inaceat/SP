2ab073ad703c78fa77786ce48ad389ca19f79f2f
Inaceat <inaceat@gmail.com>
2019-12-10 12:05:12 +0300

    [7a]Server registration MVP added.

--- Lab7a/src/Server.hpp
+++ Lab7a/src/Server.hpp
@@ -1,23 +1,52 @@
 #pragma once
 
+#include "SocketsTCP.hpp"
+#include "SocketsUDP.hpp"
+#include "NetworkMessage.hpp"
+
+
 
 namespace TTT
 {
 	class Server
 	{
 	public:
-		Server()
-		{
-
-		}
+		Server() :
+			_registrationReceiver("42042")
+		{}
 
 
 		void Start()
 		{
+			_isWorking = true;
+			_workerThread = std::thread([this]() { this->ProcessMessages(); });
+		}
 
+		void ProcessMessages()
+		{
+			int receiveTimeout = 100;
+
+			sockaddr_in senderAddress;
+
+			while (_isWorking)
+			{
+				NetworkMessage* message = _registrationReceiver.TryReceive(receiveTimeout, senderAddress);
+
+				if (nullptr != message)
+				{
+					_tempSocket = ClientSocketTCP<NetworkMessage>("127.0.0.1:42042");
+
+					Sleep(1000);
+				}
+			}
 		}
 
 	private:
+		bool _isWorking;
+		std::thread _workerThread;
 
+		BroadcastReceiverSocketUDP<NetworkMessage> _registrationReceiver;
+
+		ClientSocketTCP<NetworkMessage> _tempSocket;
 	};
 }
