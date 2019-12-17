8e4b333f4e3164ff791e0268d7c02b0a6307d1e1
Inaceat <inaceat@gmail.com>
2019-12-17 15:41:07 +0300

    [10]GetStringFromFile implemented in B library.

--- Lab10bLib/src/Lab10bLib.cpp
+++ Lab10bLib/src/Lab10bLib.cpp
@@ -4,3 +4,26 @@
 #include "stdafx.h"
 
 
+extern "C" __declspec(dllexport) std::string GetStringFromFile(std::string fileName, int positionInFile, int stringLength)
+{
+	//Get data from file
+	HANDLE tempSourceFile = CreateFileA(fileName.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+	SetFilePointer(tempSourceFile, positionInFile, nullptr, FILE_BEGIN);
+
+
+	char* buffer = new char[stringLength];
+	DWORD charsCount;
+
+	ReadFile(tempSourceFile, buffer, stringLength, &charsCount, nullptr);
+
+
+	std::string result(buffer, stringLength);
+
+	//Clear
+	CloseHandle(tempSourceFile);
+	delete[] buffer;
+
+
+	return result;
+}
