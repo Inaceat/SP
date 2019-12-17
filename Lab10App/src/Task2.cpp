064f6e2faf22b6a36527afc0f7aa2e4ea5352613
Inaceat <inaceat@gmail.com>
2019-12-17 16:03:26 +0300

    [10][WIP]Moving

--- Lab10App/src/Task2.cpp
+++ Lab10App/src/Task2.cpp
@@ -4,7 +4,16 @@
 #include "Tasks.hpp"
 
 
+__declspec(dllimport) std::string GetStringFromFile(std::string fileName, int positionInFile, int stringLength);
+
+
 void Task2::Do()
 {
-    
+	std::string fileName = "data/data.txt";
+	int positionInFile = 12;
+	int stringLength = 12;
+
+	std::string result = GetStringFromFile(fileName, positionInFile, stringLength);
+
+	std::cout << "String from file is \"" << result << "\"" << std::endl;
 }
--- Lab10bLib/src/Lab10bLib.cpp
+++ Lab10bLib/src/Lab10bLib.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 
 
-extern "C" __declspec(dllexport) std::string GetStringFromFile(std::string fileName, int positionInFile, int stringLength)
+__declspec(dllexport) std::string GetStringFromFile(std::string fileName, int positionInFile, int stringLength)
 {
 	//Get data from file
 	HANDLE tempSourceFile = CreateFileA(fileName.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
